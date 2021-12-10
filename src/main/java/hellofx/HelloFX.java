bf81cba191672b849cb89f50acd7bac0cee9cb9c
MokonaNico <mine-nico@hotmail.com>
2021-12-10 17:43:41 +0100

    3d test

--- src/main/java/hellofx/HelloFX.java
+++ src/main/java/hellofx/HelloFX.java
@@ -1,22 +1,48 @@
 package hellofx;
 
 import javafx.application.Application;
+import javafx.application.ConditionalFeature;
+import javafx.application.Platform;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.CullFace;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
-
 
 public class HelloFX extends Application {
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        boolean is3DSupported = Platform.isSupported(ConditionalFeature.SCENE3D);
+        if(!is3DSupported) {
+            System.out.println("Sorry, 3D is not supported in JavaFX on this platform.");
+            return;
+        }
+
+        Box box = new Box(100,100,100);
+        box.setCullFace(CullFace.NONE);
+        box.setTranslateX(250);
+        box.setTranslateY(100);
+        box.setTranslateZ(400);
+
+        boolean fixedEyeAtCameraZero = false;
+        PerspectiveCamera camera = new PerspectiveCamera(fixedEyeAtCameraZero);
+        camera.setTranslateX(150);
+        camera.setTranslateY(-100);
+        camera.setTranslateZ(250);
+
+        Group root = new Group(box);
+        root.setRotationAxis(Rotate.X_AXIS);
+        root.setRotate(30);
+
+        Scene scene = new Scene(root, 500, 300, true);
+        scene.setCamera(camera);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("3D Example");
+
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
